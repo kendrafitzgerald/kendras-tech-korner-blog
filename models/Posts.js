58777482433648ac2a1938f987ff3c2de72c7876
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-28 09:55:19 -0400

    added posts model

---
+++ models/Posts.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Posts extends Model {}
+
+Posts.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        post_text: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+        comments_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'comments',
+                key: 'id'
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts',
+      },
+);
+
+module.export = Posts;
