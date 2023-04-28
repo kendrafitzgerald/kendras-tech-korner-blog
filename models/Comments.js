975416720e6f6a951603e93190109b71b25bec4f
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-28 09:42:15 -0400

    added comments model

---
+++ models/Comments.js
@@ -0,0 +1,37 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Comments extends Model {}
+
+Comments.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            validate: {
+                max: 500
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+      },
+);
+
+module.export = Comments;
