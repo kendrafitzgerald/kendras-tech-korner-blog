2a5cc824e1b24d83854fde34c36b009c9b64b884
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-02 18:06:39 -0400

    tried to fix jaws

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+let sequelize 
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -15,9 +15,9 @@ if (process.env.JAWSDB_URL) {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-    },
-  );
+    }
+  )
 }
 
 
-module.exports = sequelize;
+module.exports = sequelize
