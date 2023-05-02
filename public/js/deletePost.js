d349b0389fcaadd8bacd6b5a741e7259b4365b28
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-02 17:31:15 -0400

    added delete functionality, need to work out bugs

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,1 +1,17 @@
 const deleteButton = document.querySelector('#delete-post')
+
+deletePostHandler = async (event) => {
+    event.preventDefault();
+    const id = window.location.toString().split("/")[window.location.toString().split("/").length-1]
+    const response = await fetch (`/api/post/${id}`, {
+        method: DELETE
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to delete post')
+    }
+
+};
+
+deleteButton.addEventListener('click', deletePostHandler)
