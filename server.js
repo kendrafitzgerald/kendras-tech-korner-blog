85bbe5f0355b93d9aa638e5dd2c86fea16d7a0d3
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-03 09:12:41 -0400

    tried to fix server issues'

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
+require('dotenv').config()
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
